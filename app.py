5515ff80b7d9273170cf301c3531579ca37538a3
ofmorales2135-boop <ofmorales2135@gmail.com>
2025-11-10 12:22:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', message='Hello from cicdinclass')
+
+@app.route('/api')
+def api():
+    return jsonify(message='Hello from cicdinclass')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
